8fcf8932f0f668e3e7304fcb3779363732b29e3f
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-04-18 14:08:24 +0330

    layout scroll bugfix.

--- src/@core/layouts/AdminLayout.tsx
+++ src/@core/layouts/AdminLayout.tsx
@@ -36,32 +36,34 @@ export default function DashboardLayout({
       <div className="flex flex-col h-screen bg-white">
         <div className="flex flex-1 h-auto overflow-hidden">
           <Sidebar menus={menus} />
-          <div className="max-w-7xl relative flex flex-col w-full h-auto px-4 py-6 mx-auto overflow-auto">
-            <div className="flex items-center mb-3">
-              <Breadcrumb />
-              <form autoComplete="off" action="" className="mr-auto">
-                <div className="input-inset">
-                  <div className="input-element">
-                    <IconSearch className="w-4 h-4 text-gray-400" />
+          <div className="overscroll-contain relative flex flex-col w-full h-auto px-4 py-6 overflow-auto">
+            <div className="max-w-7xl flex flex-col w-full mx-auto">
+              <div className="flex items-center mb-3">
+                <Breadcrumb />
+                <form autoComplete="off" action="" className="mr-auto">
+                  <div className="input-inset">
+                    <div className="input-element">
+                      <IconSearch className="w-4 h-4 text-gray-400" />
+                    </div>
+                    <input
+                      type="text"
+                      className="input-field"
+                      placeholder="جستجو..."
+                      autoComplete="off"
+                      name="search"
+                      tabIndex={-1}
+                      role="presentation"
+                    />
+                    <div className="input-element" dir="ltr">
+                      <span className="text-n-gray-500 font-sans text-sm">
+                        ⌘K
+                      </span>
+                    </div>
                   </div>
-                  <input
-                    type="text"
-                    className="input-field"
-                    placeholder="جستجو..."
-                    autoComplete="off"
-                    name="search"
-                    tabIndex={-1}
-                    role="presentation"
-                  />
-                  <div className="input-element" dir="ltr">
-                    <span className="text-n-gray-500 font-sans text-sm">
-                      ⌘K
-                    </span>
-                  </div>
-                </div>
-              </form>
+                </form>
+              </div>
+              {children}
             </div>
-            {children}
           </div>
         </div>
       </div>
