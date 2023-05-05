9d6d006399493885469ef959b55f08d2d9985fd4
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-05-05 14:53:41 +0330

    get ride of noqte/ui

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,52 +1,52 @@
-import DashboardLayout from "@/@core/layouts/DashboardLayout";
-import { NextPageWithLayout } from "@/pages/_app";
-import { IconAsterisk } from "@tabler/icons-react";
-import { ReactElement } from "react";
+import DashboardLayout from "@/@core/layouts/DashboardLayout"
+import { NextPageWithLayout } from "@/pages/_app"
+import { IconAsterisk } from "@tabler/icons-react"
+import { ReactElement } from "react"
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
-      <div className="flex flex-col flex-1">
-        <h1 className="text-n-gray-800 mb-8 text-3xl font-black">تنظیمات</h1>
+      <div className="flex flex-1 flex-col">
+        <h1 className="mb-8 text-3xl font-black text-n-gray-800">تنظیمات</h1>
 
-        <div className="border-n-gray-200 p-5 bg-white border rounded shadow-sm">
+        <div className="rounded border border-n-gray-200 bg-white p-5 shadow-sm">
           <div className="flex gap-6">
             <div className="w-1/3">
               <div className="flex items-center gap-2">
-                <IconAsterisk className="text-n-gray-500 w-4 h-4" />
-                <span className="text-n-gray-800 font-semibold">کلمه عبور</span>
+                <IconAsterisk className="h-4 w-4 text-n-gray-500" />
+                <span className="font-semibold text-n-gray-800">کلمه عبور</span>
               </div>
             </div>
             <div className="w-2/3">
               <div className="flex flex-col gap-6">
                 <label htmlFor="" className="flex flex-col space-y-1">
-                  <span className="text-n-gray-600 font-semibold">
+                  <span className="font-semibold text-n-gray-600">
                     کلمه عبور فعلی
                   </span>
                   <input
                     type="password"
                     placeholder="کلمه عبور فعلی خود را وارد کنید"
-                    className="border-n-gray-300 shadow-n-sm focus:outline-none focus:ring focus:ring-n-blue-50 focus:border-n-blue-300 block w-full px-3 py-2 transition-all duration-200 bg-white border rounded"
+                    className="block w-full rounded border border-n-gray-300 bg-white px-3 py-2 shadow-sm transition-all duration-200 focus:border-n-blue-300 focus:outline-none focus:ring focus:ring-n-blue-50"
                   />
                 </label>
                 <label htmlFor="" className="flex flex-col space-y-1">
-                  <span className="text-n-gray-600 font-semibold">
+                  <span className="font-semibold text-n-gray-600">
                     کلمه عبور جدید
                   </span>
                   <input
                     type="password"
                     placeholder="کلمه عبور جدید را وارد کنید"
-                    className="border-n-gray-300 shadow-n-xs focus:outline-none focus:ring focus:ring-n-blue-50 focus:border-n-blue-300 block w-full px-3 py-2 transition-all duration-200 bg-white border rounded"
+                    className="shadow-xs block w-full rounded border border-n-gray-300 bg-white px-3 py-2 transition-all duration-200 focus:border-n-blue-300 focus:outline-none focus:ring focus:ring-n-blue-50"
                   />
                 </label>
                 <label htmlFor="" className="flex flex-col space-y-1">
-                  <span className="text-n-gray-600 font-semibold">
+                  <span className="font-semibold text-n-gray-600">
                     تکرار کلمه عبور جدید
                   </span>
                   <input
                     type="password"
                     placeholder="کلمه عبور جدید را مجدد وارد کنید"
-                    className="border-n-gray-300 shadow-n-sm focus:outline-none focus:ring focus:ring-n-blue-50 focus:border-n-blue-300 block w-full px-3 py-2 transition-all duration-200 bg-white border rounded"
+                    className="block w-full rounded border border-n-gray-300 bg-white px-3 py-2 shadow-sm transition-all duration-200 focus:border-n-blue-300 focus:outline-none focus:ring focus:ring-n-blue-50"
                   />
                 </label>
               </div>
@@ -54,16 +54,16 @@ const Home: NextPageWithLayout = () => {
           </div>
         </div>
 
-        <div className="flex justify-end mt-8">
+        <div className="mt-8 flex justify-end">
           <button className="btn btn-primary">ذخیره</button>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
 Home.getLayout = function getLayout(page: ReactElement) {
-  return <DashboardLayout>{page}</DashboardLayout>;
-};
+  return <DashboardLayout>{page}</DashboardLayout>
+}
 
-export default Home;
+export default Home
