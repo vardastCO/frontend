8c98da5bf55110172d760ec6084201d91e00b4fb
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-05-27 19:38:02 +0330

    area create modal.

--- src/app/admin/locations/components/AreaCard.tsx
+++ src/app/admin/locations/components/AreaCard.tsx
@@ -2,10 +2,16 @@
 
 import { Area } from "@/generated"
 
+import { Button } from "@core/components/Button"
+import { Item } from "@core/components/Collection"
+import { Menu, MenuTrigger } from "@core/components/Menu"
+import { Popover } from "@core/components/Popover"
+import { Separator } from "@core/components/Separator"
 import { Switch } from "@core/components/Switch"
-import { useAtom } from "jotai"
+import { IconDots, IconEdit, IconTrash } from "@tabler/icons-react"
+import { useSetAtom } from "jotai"
 import useTranslation from "next-translate/useTranslation"
-import { useContext, useState } from "react"
+import { Key, useContext, useState } from "react"
 import { LocationsContext } from "./LocationsProvider"
 
 interface AreaCardProps {
@@ -16,10 +22,23 @@ interface AreaCardProps {
 }
 
 const AreaCard = ({ area }: AreaCardProps) => {
-  const { removeStateAtom } = useContext(LocationsContext)
-  const [removeState, setRemoveState] = useAtom(removeStateAtom)
+  const { removeStateAtom, entityToRemoveAtom } = useContext(LocationsContext)
+  const setEntityToRemove = useSetAtom(entityToRemoveAtom)
+  const setRemoveState = useSetAtom(removeStateAtom)
   const { t } = useTranslation()
   const { name, slug, isActive } = area
+
+  const onAction = (key: Key) => {
+    switch (key) {
+      case "remove":
+        setEntityToRemove({
+          type: "area",
+          entity: area
+        })
+        setRemoveState(true)
+        break
+    }
+  }
 
   const [active, setActive] = useState(isActive)
   return (
@@ -28,7 +47,25 @@ const AreaCard = ({ area }: AreaCardProps) => {
       <div className="mr-auto flex items-center gap-2">
         <Switch onChange={setActive} isSelected={active} size="small">
           {t("common:is_active")}
-        </Switch>
+        </Switch>{" "}
+        <MenuTrigger>
+          <Button intent="ghost" iconOnly>
+            <IconDots className="icon" />
+          </Button>
+          <Popover>
+            <Menu onAction={onAction}>
+              <Item id="edit">
+                <IconEdit className="dropdown-menu-item-icon" />
+                ویرایش
+              </Item>
+              <Separator />
+              <Item id="remove" className="danger">
+                <IconTrash className="dropdown-menu-item-icon" />
+                حذف
+              </Item>
+            </Menu>
+          </Popover>
+        </MenuTrigger>
       </div>
     </div>
   )
