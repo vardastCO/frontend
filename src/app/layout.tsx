0bda444f88e5a72e11141030810570a1ec3ee861
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-05-28 10:47:33 +0330

    nextjs-toploader added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import NextAuthProvider from "@core/providers/NextAuthProvider"
 import ReactAriaSSRProvider from "@core/providers/ReactAriaSSRProvider"
 import ReactQueryProvider from "@core/providers/ReactQueryProvider"
 import useTranslation from "next-translate/useTranslation"
+import NextTopLoader from "nextjs-toploader"
 
 export default function RootLayout({
   children
@@ -16,6 +17,11 @@ export default function RootLayout({
     <ReactAriaSSRProvider>
       <html lang={lang}>
         <body>
+          <NextTopLoader
+            color="#030712"
+            showSpinner={false}
+            shadow="0 0 10px #030712,0 0 5px #030712"
+          />
           <NextAuthProvider>
             <ReactQueryProvider>
               {children}
