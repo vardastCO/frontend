0d3f61d161aff2cb6c0bf05c1b802ebfc6649745
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-06-20 18:41:52 +0330

    categories.

--- src/app/admin/products/new/components/ProductForm.tsx
+++ src/app/admin/products/new/components/ProductForm.tsx
@@ -2,15 +2,21 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import {
+  IconCheck,
   IconGift,
   IconPackage,
   IconPackages,
+  IconSelector,
   IconWorld
 } from "@tabler/icons-react"
 import useTranslation from "next-translate/useTranslation"
 import { useForm } from "react-hook-form"
 import { TypeOf, z } from "zod"
 
+import { useGetVocabularyQuery } from "@/generated"
+
+import graphqlRequestClient from "@core/clients/graphqlRequestClient"
+import { mergeClasses } from "@core/utils/mergeClasses"
 import { slugInputSchema } from "@core/utils/zodValidationSchemas"
 import Dropzone from "@core/components/Dropzone"
 import {
@@ -22,7 +28,19 @@ import {
   FormMessage
 } from "@core/components/react-hook-form/form"
 import { Button } from "@core/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem
+} from "@core/components/ui/command"
 import { Input } from "@core/components/ui/input"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from "@core/components/ui/popover"
 import { RadioGroup, RadioGroupItem } from "@core/components/ui/radio-group"
 import { Switch } from "@core/components/ui/switch"
 
@@ -32,11 +50,15 @@ const ProductForm = () => {
     productName: z.string(),
     productType: z.string(),
     isActive: z.boolean(),
+    categoryId: z.number(),
     pageTitle: z.string(),
     metaDescription: z.string(),
     slug: slugInputSchema
   })
   type CreateProductType = TypeOf<typeof CreateProductSchema>
+  const { data } = useGetVocabularyQuery(graphqlRequestClient, {
+    slug: "product_categories"
+  })
 
   const form = useForm<CreateProductType>({
     resolver: zodResolver(CreateProductSchema),
@@ -71,7 +93,10 @@ const ProductForm = () => {
                   <FormItem>
                     <FormLabel>{t("common:product_name")}</FormLabel>
                     <FormControl size="large">
-                      <Input {...field} />
+                      <Input
+                        {...field}
+                        placeholder={t("common:enter_product_name")}
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -198,6 +223,75 @@ const ProductForm = () => {
                 )}
               />
               {/* TODO: Categories */}
+              <FormField
+                control={form.control}
+                name="categoryId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:category")}</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            noStyle
+                            role="combobox"
+                            className="input-field flex items-center text-start"
+                          >
+                            {field.value
+                              ? data?.vocabulary.categories.find(
+                                  (category) =>
+                                    category && category.id === field.value
+                                )?.title
+                              : t("common:choose_entity", {
+                                  entity: t("common:category")
+                                })}
+                            <IconSelector className="ms-auto h-4 w-4 shrink-0" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="p-0">
+                        <Command>
+                          <CommandInput
+                            placeholder={t("common:search_entity", {
+                              entity: t("common:category")
+                            })}
+                          />
+                          <CommandEmpty>
+                            {t("common:no_entity_found", {
+                              entity: t("common:category")
+                            })}
+                          </CommandEmpty>
+                          <CommandGroup className="max-h-[150px] overflow-auto">
+                            {data?.vocabulary.categories.map(
+                              (category) =>
+                                category && (
+                                  <CommandItem
+                                    value={category.title}
+                                    key={category.id}
+                                    onSelect={(value) => {
+                                      form.setValue("categoryId", +value)
+                                    }}
+                                  >
+                                    <IconCheck
+                                      className={mergeClasses(
+                                        "mr-2 h-4 w-4",
+                                        category.id === field.value
+                                          ? "opacity-100"
+                                          : "opacity-0"
+                                      )}
+                                    />
+                                    {category.title}
+                                  </CommandItem>
+                                )
+                            )}
+                          </CommandGroup>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               {/* TODO: images */}
               <Dropzone />
             </div>
