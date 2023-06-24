38eaac30a705c8bff59293924f31d8aa99c5a077
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-06-24 19:10:16 +0330

    brand form.

--- src/app/admin/brands/new/components/CreateBrand.tsx
+++ src/app/admin/brands/new/components/CreateBrand.tsx
@@ -9,6 +9,7 @@ import { TypeOf, z } from "zod"
 import { useCreateBrandMutation } from "@/generated"
 
 import graphqlRequestClient from "@core/clients/graphqlRequestClient"
+import { socialNetworks } from "@core/utils/socialNetworks"
 import zodI18nMap from "@core/utils/zodErrorMap"
 import { slugInputSchema } from "@core/utils/zodValidationSchemas"
 import {
@@ -19,8 +20,17 @@ import {
   FormLabel,
   FormMessage
 } from "@core/components/react-hook-form/form"
+import Card from "@core/components/shared/Card"
 import { Button } from "@core/components/ui/button"
 import { Input } from "@core/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@core/components/ui/select"
+import { Textarea } from "@core/components/ui/textarea"
 import { useToast } from "@core/hooks/use-toast"
 
 const CreateBrand = () => {
@@ -43,7 +53,13 @@ const CreateBrand = () => {
   z.setErrorMap(zodI18nMap)
   const CreateBrandSchema = z.object({
     name: z.string(),
-    slug: slugInputSchema
+    slug: slugInputSchema,
+    email: z.string().email().optional(),
+    phone: z.string().optional(),
+    address: z.string().optional(),
+    website: z.string().optional(),
+    about: z.string().optional(),
+    social: z.string().optional()
   })
   type CreateBrandType = TypeOf<typeof CreateBrandSchema>
 
@@ -55,7 +71,7 @@ const CreateBrand = () => {
   const name = form.watch("name")
 
   function onSubmit(data: CreateBrandType) {
-    const { name, slug } = data
+    const { name, slug, email, phone, address, website, about } = data
 
     createBrandMutation.mutate({
       createBrandInput: {
@@ -83,33 +99,159 @@ const CreateBrand = () => {
             {t("common:save_entity", { entity: t("common:brand") })}
           </Button>
         </div>
-        <div className="flex flex-col gap-6">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{t("common:name")}</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="slug"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{t("common:slug")}</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <div className="flex flex-col gap-8">
+          <Card template="1/2" title={t("common:title")}>
+            <div className="grid grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:name")}</FormLabel>
+                    <FormControl>
+                      <Input type="text" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="slug"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:slug")}</FormLabel>
+                    <FormControl>
+                      <Input type="text" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </Card>
+
+          <Card template="1/2" title={t("common:contact_information")}>
+            <div className="grid grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem className="col-span-full">
+                    <FormLabel>{t("common:address")}</FormLabel>
+                    <FormControl>
+                      <Input type="text" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:phone")}</FormLabel>
+                    <FormControl>
+                      <Input type="tel" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:email")}</FormLabel>
+                    <FormControl>
+                      <Input type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="website"
+                render={({ field }) => (
+                  <FormItem className="col-span-full">
+                    <FormLabel>{t("common:website")}</FormLabel>
+                    <FormControl dir="ltr" prefixAddon="http://">
+                      <Input type="text" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </Card>
+
+          <Card template="1/2" title={t("common:about")}>
+            <div className="grid grid-cols-1 gap-6">
+              <FormField
+                control={form.control}
+                name="about"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:about")}</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </Card>
+
+          <Card template="1/2" title={t("common:social_networks")}>
+            <div className="grid grid-cols-[1fr_2fr] gap-6">
+              <FormField
+                control={form.control}
+                name="social"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:social_network")}</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue
+                            placeholder={t("common:select_placeholder")}
+                          />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {socialNetworks.map((item, idx) => (
+                          <SelectItem value={item.name} key={idx}>
+                            {item.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:address")}</FormLabel>
+                    <FormControl>
+                      <Input type="text" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </Card>
         </div>
       </form>
     </Form>
--- src/app/admin/users/components/UserEditForm.tsx
+++ src/app/admin/users/components/UserEditForm.tsx
@@ -18,6 +18,7 @@ import {
   FormLabel,
   FormMessage
 } from "@core/components/react-hook-form/form"
+import Card from "@core/components/shared/Card"
 import { Button } from "@core/components/ui/button"
 import {
   Command,
@@ -79,240 +80,219 @@ const UserEditForm = ({ user }: Props) => {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} noValidate>
         <div className="flex flex-col gap-8">
-          <div className="card flex items-start rounded p-4">
-            <div className="w-1/3">
-              <h2 className="font-medium text-gray-800">
-                {t("common:personal_information")}
-              </h2>
+          <Card template="1/2" title={t("common:personal_information")}>
+            <div className="grid grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:first_name")}</FormLabel>
+                    <FormControl>
+                      <Input type="text" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:last_name")}</FormLabel>
+                    <FormControl>
+                      <Input type="text" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="col-span-2">
+                    <FormLabel>{t("common:email")}</FormLabel>
+                    <FormControl>
+                      <Input type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="cellphone"
+                render={({ field }) => (
+                  <FormItem className="col-span-2">
+                    <FormLabel>{t("common:cellphone")}</FormLabel>
+                    <FormControl>
+                      <Input type="phone" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-            <div className="w-2/3">
-              <div className="grid grid-cols-2 gap-6">
-                <FormField
-                  control={form.control}
-                  name="firstName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("common:first_name")}</FormLabel>
-                      <FormControl>
-                        <Input type="text" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="lastName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("common:last_name")}</FormLabel>
-                      <FormControl>
-                        <Input type="text" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="col-span-2">
-                      <FormLabel>{t("common:email")}</FormLabel>
-                      <FormControl>
-                        <Input type="email" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="cellphone"
-                  render={({ field }) => (
-                    <FormItem className="col-span-2">
-                      <FormLabel>{t("common:cellphone")}</FormLabel>
-                      <FormControl>
-                        <Input type="phone" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+          </Card>
+          <Card template="1/2" title={t("common:settings")}>
+            <div className="grid grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="country"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:country")}</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            noStyle
+                            role="combobox"
+                            className="input-field flex items-center text-start"
+                          >
+                            {field.value
+                              ? countries?.countries.find(
+                                  (country) => country.slug === field.value
+                                )?.name
+                              : "Select language"}
+                            <IconSelector className="ms-auto h-4 w-4 shrink-0" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="p-0">
+                        <Command>
+                          <CommandInput placeholder="Search framework..." />
+                          <CommandEmpty>No framework found.</CommandEmpty>
+                          <CommandGroup className="max-h-[100px] overflow-auto">
+                            {countries?.countries.map((country) => (
+                              <CommandItem
+                                value={country.slug}
+                                key={country.id}
+                                onSelect={(value) => {
+                                  form.setValue("country", value)
+                                }}
+                              >
+                                <IconCheck
+                                  className={mergeClasses(
+                                    "mr-2 h-4 w-4",
+                                    country.slug === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                                {country.name}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="timezone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:timezone")}</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            noStyle
+                            role="combobox"
+                            className="input-field flex items-center text-start"
+                          >
+                            {field.value
+                              ? timezones.find(
+                                  (timezon) => timezon.title === field.value
+                                )?.title
+                              : "Select language"}
+                            <IconSelector className="ms-auto h-4 w-4 shrink-0" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="p-0">
+                        <Command>
+                          <CommandInput placeholder="Search framework..." />
+                          <CommandEmpty>No framework found.</CommandEmpty>
+                          <CommandGroup>
+                            {timezones.map((timezone) => (
+                              <CommandItem
+                                value={timezone.title}
+                                key={timezone.id}
+                                onSelect={(value) => {
+                                  form.setValue("timezone", value)
+                                }}
+                              >
+                                <IconCheck
+                                  className={mergeClasses(
+                                    "mr-2 h-4 w-4",
+                                    timezone.title === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                                {timezone.title}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-          </div>
-          <div className="card flex items-start rounded p-4">
-            <div className="w-1/3">
-              <h2 className="font-medium text-gray-800">
-                {t("common:settings")}
-              </h2>
+          </Card>
+          <Card template="1/2" title={t("common:password")}>
+            <div className="grid grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:new_password")}</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="repeatPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:repeat_password")}</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="currentPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("common:current_password")}</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-            <div className="w-2/3">
-              <div className="grid grid-cols-2 gap-6">
-                <FormField
-                  control={form.control}
-                  name="country"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("common:country")}</FormLabel>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              noStyle
-                              role="combobox"
-                              className="input-field flex items-center text-start"
-                            >
-                              {field.value
-                                ? countries?.countries.find(
-                                    (country) => country.slug === field.value
-                                  )?.name
-                                : "Select language"}
-                              <IconSelector className="ms-auto h-4 w-4 shrink-0" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="p-0">
-                          <Command>
-                            <CommandInput placeholder="Search framework..." />
-                            <CommandEmpty>No framework found.</CommandEmpty>
-                            <CommandGroup className="max-h-[100px] overflow-auto">
-                              {countries?.countries.map((country) => (
-                                <CommandItem
-                                  value={country.slug}
-                                  key={country.id}
-                                  onSelect={(value) => {
-                                    form.setValue("country", value)
-                                  }}
-                                >
-                                  <IconCheck
-                                    className={mergeClasses(
-                                      "mr-2 h-4 w-4",
-                                      country.slug === field.value
-                                        ? "opacity-100"
-                                        : "opacity-0"
-                                    )}
-                                  />
-                                  {country.name}
-                                </CommandItem>
-                              ))}
-                            </CommandGroup>
-                          </Command>
-                        </PopoverContent>
-                      </Popover>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="timezone"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("common:timezone")}</FormLabel>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              noStyle
-                              role="combobox"
-                              className="input-field flex items-center text-start"
-                            >
-                              {field.value
-                                ? timezones.find(
-                                    (timezon) => timezon.title === field.value
-                                  )?.title
-                                : "Select language"}
-                              <IconSelector className="ms-auto h-4 w-4 shrink-0" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="p-0">
-                          <Command>
-                            <CommandInput placeholder="Search framework..." />
-                            <CommandEmpty>No framework found.</CommandEmpty>
-                            <CommandGroup>
-                              {timezones.map((timezone) => (
-                                <CommandItem
-                                  value={timezone.title}
-                                  key={timezone.id}
-                                  onSelect={(value) => {
-                                    form.setValue("timezone", value)
-                                  }}
-                                >
-                                  <IconCheck
-                                    className={mergeClasses(
-                                      "mr-2 h-4 w-4",
-                                      timezone.title === field.value
-                                        ? "opacity-100"
-                                        : "opacity-0"
-                                    )}
-                                  />
-                                  {timezone.title}
-                                </CommandItem>
-                              ))}
-                            </CommandGroup>
-                          </Command>
-                        </PopoverContent>
-                      </Popover>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="card flex items-start rounded p-4">
-            <div className="w-1/3">
-              <h2 className="font-medium text-gray-800">
-                {t("common:password")}
-              </h2>
-            </div>
-            <div className="w-2/3">
-              <div className="grid grid-cols-2 gap-6">
-                <FormField
-                  control={form.control}
-                  name="newPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("common:new_password")}</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="password" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="repeatPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("common:repeat_password")}</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="password" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="currentPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("common:current_password")}</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="password" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-          </div>
+          </Card>
 
           <div className="flex items-center justify-end gap-2">
             <Button type="submit" disabled={form.formState.isSubmitting}>
