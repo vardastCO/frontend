60f16e696f67db609b7f3c78eece6fa9fb84e64c
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-07-02 15:46:23 +0330

    products url redirect solution.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,28 @@
 import { NextRequest, NextResponse } from "next/server"
 import i18n from "i18n"
 import { withAuth } from "next-auth/middleware"
+import { pathToRegexp } from "path-to-regexp"
 
 export default withAuth(
-  function middleware(request: NextRequest) {
+  async function middleware(request: NextRequest) {
     const locale = request.nextUrl.locale || i18n.defaultLocale
     request.nextUrl.searchParams.set("lang", locale)
     request.nextUrl.href = request.nextUrl.href.replace(`/${locale}`, "")
+
+    const productPathRegexp = pathToRegexp("/p/:slug1/:slug2?")
+    const productPathRegexpText = productPathRegexp.exec(
+      request.nextUrl.pathname
+    )
+    if (productPathRegexpText) {
+      const id = productPathRegexpText[1]
+      const name = productPathRegexpText[2]
+      if (!name) {
+        return NextResponse.redirect(
+          new URL(`/p/${id}/${"hello"}`, request.url),
+          301
+        )
+      }
+    }
 
     return NextResponse.rewrite(request.nextUrl.href)
   },
