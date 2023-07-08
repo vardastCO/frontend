3712fde74fd3a468ef2a432cf667e25434b33a1b
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-07-08 18:49:02 +0330

    fix.

--- src/@core/components/shared/UserMenu.tsx
+++ src/@core/components/shared/UserMenu.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { useRef, useState } from "react"
+import { useState } from "react"
+import { useClickOutside } from "@mantine/hooks"
 import {
   IconLogout,
   IconMoon,
@@ -11,7 +12,6 @@ import clsx from "clsx"
 import { signOut, useSession } from "next-auth/react"
 import { useTheme } from "next-themes"
 import useTranslation from "next-translate/useTranslation"
-import { useOnClickOutside } from "usehooks-ts"
 
 import { Button } from "@core/components/ui/button"
 
@@ -20,10 +20,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 const UserMenu = () => {
   const { t } = useTranslation()
   const { theme, setTheme } = useTheme()
-  const wrapperRef = useRef<HTMLDivElement>(null)
   const { data: session } = useSession()
   const [open, setOpen] = useState<boolean>(false)
-  useOnClickOutside(wrapperRef, () => {
+  const ref = useClickOutside(() => {
     if (open) setOpen(false)
   })
 
@@ -37,72 +36,71 @@ const UserMenu = () => {
     setTheme(newTheme)
   }
 
-  if (session)
-    return (
-      <div ref={wrapperRef} className={clsx(["w-full", open && "-mb-2"])}>
-        <div
-          className={clsx([
-            open ? "card -mx-2 flex flex-col gap-3 rounded p-2" : ""
-          ])}
+  return session ? (
+    <div ref={ref} className={clsx(["w-full", open && "-mb-2"])}>
+      <div
+        className={clsx([
+          open ? "card -mx-2 flex flex-col gap-3 rounded p-2" : ""
+        ])}
+      >
+        {open && (
+          <div className="flex flex-col gap-1">
+            <Button
+              onClick={() => toggleTeheme()}
+              variant="ghost"
+              className="justify-start text-start"
+            >
+              <>
+                {theme === "dark" ? (
+                  <IconSun className="icon" />
+                ) : (
+                  <IconMoon className="icon" />
+                )}
+                {theme === "dark"
+                  ? t("common:switch_dark_mode_off")
+                  : t("common:switch_dark_mode_on")}
+              </>
+            </Button>
+            <Button
+              onClick={() => signOut()}
+              variant="ghost"
+              className="justify-start text-start"
+            >
+              <>
+                <IconLogout className="icon" />
+                {t("common:logout")}
+              </>
+            </Button>
+          </div>
+        )}
+        <Button
+          onClick={() => toggle()}
+          noStyle
+          className="flex w-full items-center gap-2 text-start outline-none focus-visible:outline-none"
         >
-          {open && (
-            <div className="flex flex-col gap-1">
-              <Button
-                onClick={() => toggleTeheme()}
-                variant="ghost"
-                className="justify-start text-start"
-              >
-                <>
-                  {theme === "dark" ? (
-                    <IconSun className="icon" />
-                  ) : (
-                    <IconMoon className="icon" />
-                  )}
-                  {theme === "dark"
-                    ? t("common:switch_dark_mode_off")
-                    : t("common:switch_dark_mode_on")}
-                </>
-              </Button>
-              <Button
-                onClick={() => signOut()}
-                variant="ghost"
-                className="justify-start text-start"
-              >
-                <>
-                  <IconLogout className="icon" />
-                  {t("common:logout")}
-                </>
-              </Button>
-            </div>
-          )}
-          <Button
-            onClick={() => toggle()}
-            noStyle
-            className="flex w-full items-center gap-2 text-start outline-none focus-visible:outline-none"
-          >
-            <Avatar>
-              <AvatarImage
-                src={`https://api.dicebear.com/5.x/big-ears-neutral/svg?seed=
+          <Avatar>
+            <AvatarImage
+              src={`https://api.dicebear.com/5.x/big-ears-neutral/svg?seed=
             ${session?.user?.profile.fullName}`}
-                alt={session?.user?.profile.fullName || ""}
-              />
-              <AvatarFallback>{session?.user?.profile.fullName}</AvatarFallback>
-            </Avatar>
-            <div className="flex flex-1 flex-col truncate">
-              <span className="truncate font-medium text-gray-800">
-                {session?.user?.profile.fullName}
-              </span>
-              <span className="truncate text-sm text-gray-500">
-                {session?.user?.profile.email}
-              </span>
-            </div>
-            <IconSelector className="h-3 w-3 text-gray-600" />
-          </Button>
-        </div>
+              alt={session?.user?.profile.fullName || ""}
+            />
+            <AvatarFallback>{session?.user?.profile.fullName}</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-1 flex-col truncate">
+            <span className="truncate font-medium text-gray-800">
+              {session?.user?.profile.fullName}
+            </span>
+            <span className="truncate text-sm text-gray-500">
+              {session?.user?.profile.email}
+            </span>
+          </div>
+          <IconSelector className="h-3 w-3 text-gray-600" />
+        </Button>
       </div>
-    )
-
-  return <></>
+    </div>
+  ) : (
+    <></>
+  )
 }
 
 export default UserMenu
