80d9c1a204e18b5a9f6fa17d58f05cc26829f704
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-07-15 15:19:51 +0330

    not found page fix.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,20 @@
-import NotFoundPage from "@core/components/shared/NotFoundPage"
+import Link from "next/link"
 
 export default function NotFound() {
-  return <NotFoundPage />
+  return (
+    <div
+      dir="rtl"
+      className="flex h-screen w-full flex-col items-center justify-center"
+    >
+      <div>
+        <h2 className="font-bold text-gray-800">همم!</h2>
+        <p className="text-gray-700">صفحه مورد نظر شما پیدا نشد</p>
+        <div className="mt-8 flex flex-col gap-6">
+          <Link href="/" className="inline-block text-sm text-brand-500">
+            <span>خانه</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
