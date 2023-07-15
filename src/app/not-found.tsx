1ccc92b7f8ff6ba73898af0fb76688a818f5a438
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-07-15 16:14:22 +0330

    404 image.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image"
 import Link from "next/link"
+
+import image404 from "@/assets/404.svg"
 
 export default function NotFound() {
   return (
@@ -7,11 +10,16 @@ export default function NotFound() {
       className="flex h-screen w-full flex-col items-center justify-center"
     >
       <div>
+        <Image
+          src={image404}
+          alt="صفحه مورد نظر شما پیدا نشد"
+          className="mb-12"
+        />
         <h2 className="font-bold text-gray-800">همم!</h2>
         <p className="text-gray-700">صفحه مورد نظر شما پیدا نشد</p>
         <div className="mt-8 flex flex-col gap-6">
           <Link href="/" className="inline-block text-sm text-brand-500">
-            <span>خانه</span>
+            برگشت به خانه
           </Link>
         </div>
       </div>
