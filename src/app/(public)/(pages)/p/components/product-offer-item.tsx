441c60e5ab2bc6a3929f268075fff5afd328b4ec
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-07-19 18:10:38 +0330

    product offers.

---
+++ src/app/(public)/(pages)/p/components/product-offer-item.tsx
@@ -0,0 +1,106 @@
+import Link from "next/link"
+import { addCommas, digitsEnToFa } from "@persian-tools/persian-tools"
+import { IconBuildingWarehouse, IconMapPin } from "@tabler/icons-react"
+import { formatDistanceToNow, setDefaultOptions } from "date-fns"
+import { faIR } from "date-fns/locale"
+
+import { Offer, Uom } from "@/generated"
+
+import { Button } from "@core/components/ui/button"
+
+type Props = {
+  offer: Offer
+  uom: Uom
+}
+
+const ProductOfferItem = ({ offer, uom }: Props) => {
+  setDefaultOptions({
+    locale: faIR,
+    weekStartsOn: 6
+  })
+  const hasDiscount = false
+
+  return (
+    <div className="flex flex-col flex-wrap items-start gap-3 rounded-md p-4 md:flex-row lg:items-center">
+      <div className="flex items-center gap-2.5 py-3">
+        <IconBuildingWarehouse
+          className="hidden h-8 w-8 text-gray-400 md:block"
+          stroke={1.5}
+        />
+        <div className="flex flex-col items-start gap-1.5">
+          <Link
+            href={`/seller/${offer.seller.id}/${offer.seller.name}`}
+            prefetch={false}
+          >
+            <div className="font-bold text-gray-700">{offer.seller.name}</div>
+          </Link>
+          <div className="flex items-center gap-6 text-sm">
+            {/* TODO */}
+            <div className="flex items-center gap-1 text-gray-500">
+              <IconMapPin className="h-4 w-4 text-gray-400" stroke={1.5} />
+              تهران
+            </div>
+            {/* TODO */}
+            {/* <div className="flex items-center gap-1">
+              <span className="text-gray-500">عملکرد</span>
+              <span className="font-bold text-emerald-500">عالی</span>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      {offer.lastPublicConsumerPrice && (
+        <div className="flex w-full flex-col gap-2 md:mr-auto lg:w-auto">
+          <div className="flex w-full flex-wrap items-end gap-3 md:mr-auto md:gap-5 lg:w-auto">
+            <div className="flex flex-col items-stretch justify-between text-gray-800">
+              <div className="flex flex-col items-start gap-1 md:flex-row md:gap-2">
+                {hasDiscount && (
+                  <div className="rounded bg-red-500 px-2 py-1.5 text-center text-sm font-semibold leading-none text-white md:mt-2">
+                    {digitsEnToFa(15)}%
+                  </div>
+                )}
+                <div>
+                  <span className="text-xs leading-none text-gray-600">
+                    قیمت هر {uom.name}
+                  </span>
+                  <div className="flex items-center gap-1 leading-none">
+                    <span className="text-lg font-semibold leading-none">
+                      {digitsEnToFa(
+                        addCommas(offer.lastPublicConsumerPrice.amount)
+                      )}
+                    </span>
+                    <span className="text-sm leading-none">تومان</span>
+                  </div>
+                  {hasDiscount && (
+                    <div className="mt-2 flex-1">
+                      <span className="text-sm text-gray-500 line-through">
+                        {digitsEnToFa(
+                          addCommas(offer.lastPublicConsumerPrice.amount)
+                        )}
+                      </span>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <Button className="mr-auto md:mr-0">خرید از این فروشنده</Button>
+          </div>
+          <div className="mt-2 text-xs text-gray-600 lg:text-left">
+            <span>آخرین به‌روز رسانی قیمت:</span>{" "}
+            <span>
+              {digitsEnToFa(
+                formatDistanceToNow(
+                  new Date(offer.lastPublicConsumerPrice.createdAt).getTime(),
+                  {
+                    addSuffix: true
+                  }
+                )
+              )}
+            </span>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProductOfferItem
--- src/app/(public)/(pages)/p/components/product-page.tsx
+++ src/app/(public)/(pages)/p/components/product-page.tsx
@@ -5,6 +5,7 @@ import { notFound } from "next/navigation"
 import { addCommas, digitsEnToFa } from "@persian-tools/persian-tools"
 import { IconBuildingWarehouse, IconMapPin } from "@tabler/icons-react"
 import { useQuery } from "@tanstack/react-query"
+import { formatDistanceToNow } from "date-fns"
 import {
   BreadcrumbList,
   ItemList,
@@ -15,7 +16,9 @@ import {
 import {
   AttributeValue,
   GetProductQuery,
-  Image as ProductImage
+  Offer,
+  Image as ProductImage,
+  Uom
 } from "@/generated"
 
 import Breadcrumb, { CrumbItemProps } from "@core/components/shared/Breadcrumb"
@@ -23,7 +26,7 @@ import { Button } from "@core/components/ui/button"
 import { getProductQueryFn } from "@core/queryFns/productQueryFns"
 import ProductAttributes from "@/app/(public)/(pages)/p/components/product-attributes"
 import ProductImages from "@/app/(public)/(pages)/p/components/product-images"
-import ProductSellers from "@/app/(public)/(pages)/p/components/product-sellers"
+import ProductOffers from "@/app/(public)/(pages)/p/components/product-offers"
 
 type ProductPageProps = {
   isMobileView: RegExpMatchArray | null
@@ -36,8 +39,7 @@ const ProductPage = ({ id, isMobileView }: ProductPageProps) => {
     queryFn: () => getProductQueryFn(id)
   })
 
-  const hasDiscount = true
-  const price = 48899300
+  const hasDiscount = false
 
   if (!data) notFound()
 
@@ -180,87 +182,140 @@ const ProductPage = ({ id, isMobileView }: ProductPageProps) => {
               </ul>
             </div>
 
-            <div className="rounded-md border border-gray-200 p-4 lg:mt-auto">
-              <div className="mb-4 flex items-center gap-2">
-                <span className="tag tag-warning tag-light">بهترین قیمت</span>
-                <Link
-                  href="#sellers"
-                  scroll={false}
-                  className="mr-auto text-sm font-semibold text-brand-600"
-                >
-                  +۲ فروشنده دیگر
-                </Link>
-              </div>
-              <div className="divide-y divide-gray-200">
-                <div className="flex items-start gap-2.5 py-3">
-                  <IconBuildingWarehouse
-                    className="h-8 w-8 text-gray-400"
-                    stroke={1.5}
-                  />
-                  <div className="flex flex-col items-start gap-1.5">
-                    <div className="font-bold text-gray-700">فروشگاه عرفان</div>
-                    <div className="flex items-center gap-6 text-sm">
-                      <div className="flex items-center gap-1 text-gray-500">
-                        <IconMapPin
-                          className="h-4 w-4 text-gray-400"
-                          stroke={1.5}
-                        />
-                        تهران
-                      </div>
-                      <div className="flex items-center gap-1">
-                        <span className="text-gray-500">عملکرد</span>
-                        <span className="font-bold text-emerald-500">عالی</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex flex-col gap-4 pt-3">
-                  <div className="flex flex-col items-start justify-between gap-2 md:flex-row lg:items-center">
-                    <span className="font-semibold text-gray-600">
-                      قیمت فروشنده
+            {product.offers &&
+              product.offers.at(0) &&
+              product.offers.at(0)?.lastPublicConsumerPrice && (
+                <div className="rounded-md border border-gray-200 p-4 lg:mt-auto">
+                  <div className="mb-2 flex items-center gap-2 md:mb-4">
+                    <span className="tag tag-warning tag-light text-sm md:text-base">
+                      بهترین قیمت
                     </span>
-                    <div className="flex flex-col items-stretch justify-between text-gray-800">
-                      <div className="flex items-start gap-2">
-                        {hasDiscount && (
-                          <div className="mt-2 rounded bg-red-500 px-2 py-1.5 text-center text-sm font-semibold leading-none text-white">
-                            {digitsEnToFa(15)}%
+                    {product.offers.length > 1 && (
+                      <Link
+                        href="#sellers"
+                        scroll={false}
+                        className="mr-auto text-sm font-semibold text-brand-600"
+                      >
+                        +{digitsEnToFa(product.offers.length - 1)} فروشنده دیگر
+                      </Link>
+                    )}
+                  </div>
+                  <div className="divide-y divide-gray-200">
+                    <div className="flex items-start gap-2.5 py-3">
+                      <IconBuildingWarehouse
+                        className="h-8 w-8 text-gray-400"
+                        stroke={1.5}
+                      />
+                      <div className="flex flex-col items-start gap-1.5">
+                        <div className="font-bold text-gray-700">
+                          {product.offers.at(0)?.seller.name}
+                        </div>
+                        <div className="flex items-center gap-6 text-sm">
+                          {/* TODO */}
+                          <div className="flex items-center gap-1 text-gray-500">
+                            <IconMapPin
+                              className="h-4 w-4 text-gray-400"
+                              stroke={1.5}
+                            />
+                            تهران
                           </div>
-                        )}
-                        <div>
-                          <span className="text-xs leading-none text-gray-600">
-                            قیمت هر تن
+                          {/* TODO */}
+                          {/* <div className="flex items-center gap-1">
+                          <span className="text-gray-500">عملکرد</span>
+                          <span className="font-bold text-emerald-500">
+                            عالی
                           </span>
-                          <div className="flex items-center gap-1 leading-none">
-                            <span className="text-lg font-semibold leading-none">
-                              {digitsEnToFa(addCommas(price))}
-                            </span>
-                            <span className="text-sm leading-none">تومان</span>
-                          </div>
-                          <div className="mt-2 flex-1">
-                            {hasDiscount && (
-                              <span className="text-sm text-gray-500 line-through">
-                                {digitsEnToFa(addCommas(price))}
-                              </span>
-                            )}
-                          </div>
+                        </div> */}
                         </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="mr-auto">
-                    <Button size="medium">خرید از فروشگاه عرفان</Button>
+                    <div className="flex flex-col gap-4 pt-3">
+                      <div className="flex flex-col items-start justify-between gap-2 md:flex-row lg:items-center">
+                        <div>
+                          <span className="mt-2 inline-block font-semibold text-gray-600">
+                            قیمت فروشنده
+                          </span>
+                          <div className="mt-1 text-xs text-gray-600 md:mt-2 lg:text-left">
+                            <span>آخرین به‌روز رسانی قیمت:</span>{" "}
+                            <span>
+                              {digitsEnToFa(
+                                formatDistanceToNow(
+                                  new Date(
+                                    product.offers.at(0)
+                                      ?.lastPublicConsumerPrice?.createdAt
+                                  ).getTime(),
+                                  {
+                                    addSuffix: true
+                                  }
+                                )
+                              )}
+                            </span>
+                          </div>
+                        </div>
+                        <div className="flex flex-col items-stretch justify-between text-gray-800">
+                          <div className="flex items-start gap-2">
+                            {hasDiscount && (
+                              <div className="mt-2 rounded bg-red-500 px-2 py-1.5 text-center text-sm font-semibold leading-none text-white">
+                                {digitsEnToFa(15)}%
+                              </div>
+                            )}
+                            <div>
+                              <span className="text-xs leading-none text-gray-600">
+                                قیمت هر {product.uom.name}
+                              </span>
+                              <div className="flex items-center gap-1 leading-none">
+                                <span className="text-lg font-semibold leading-none">
+                                  {digitsEnToFa(
+                                    addCommas(
+                                      product.offers.at(0)
+                                        ?.lastPublicConsumerPrice?.amount || 0
+                                    )
+                                  )}
+                                </span>
+                                <span className="text-sm leading-none">
+                                  تومان
+                                </span>
+                              </div>
+                              <div className="mt-2 flex-1">
+                                {hasDiscount && (
+                                  <span className="text-sm text-gray-500 line-through">
+                                    {digitsEnToFa(
+                                      addCommas(
+                                        product.offers.at(0)
+                                          ?.lastPublicConsumerPrice?.amount || 0
+                                      )
+                                    )}
+                                  </span>
+                                )}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="md:mr-auto">
+                        <Button size="medium" fullWidth>
+                          خرید از {product.offers.at(0)?.seller.name}
+                        </Button>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              )}
           </div>
         </div>
-        <ProductSellers />
-        <ProductAttributes
-          attributes={product.attributeValues as AttributeValue[]}
-        />
+        {product.offers && (
+          <ProductOffers
+            uom={product.uom as Uom}
+            offers={product.offers as Offer[]}
+          />
+        )}
+        {product.attributeValues && (
+          <ProductAttributes
+            attributes={product.attributeValues as AttributeValue[]}
+          />
+        )}
       </div>
       <script
         type="application/ld+json"
