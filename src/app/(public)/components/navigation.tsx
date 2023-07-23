a0699ebdb9b536534aac19cf4214174556f95dcd
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-07-23 18:41:03 +0330

    navs.

--- src/app/(public)/components/navigation.tsx
+++ src/app/(public)/components/navigation.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link"
+import { useQuery } from "@tanstack/react-query"
+
+import { GetVocabularyQuery } from "@/generated"
+
+import { getVocabularyQueryFn } from "@core/queryFns/vocabularyQueryFns"
+
 const nav = [
   { title: "مصالح ساختمای" },
   { title: "تاسیسات" },
@@ -6,15 +13,28 @@ const nav = [
   { title: "آهن آلات" }
 ]
 const Navigation = () => {
+  const { data } = useQuery<GetVocabularyQuery>({
+    queryKey: ["vocabulary", { slug: "product_categories" }],
+    queryFn: () => getVocabularyQueryFn("product_categories")
+  })
+
+  if (!data) return <></>
+
   return (
-    <ol className="flex items-center gap-1">
-      {nav.map((item, idx) => (
-        <li key={idx}>
-          <a href="/search" className="inline-flex px-3 py-2">
-            {item.title}
-          </a>
-        </li>
-      ))}
+    <ol className="hide-scrollbar flex items-center gap-1 overflow-x-auto">
+      {data.vocabulary.categories.map(
+        (category) =>
+          category && (
+            <li key={category.id}>
+              <Link
+                href={`/search/${category.id}/${category.title}`}
+                className="inline-flex whitespace-nowrap px-3 py-2"
+              >
+                {category.title}
+              </Link>
+            </li>
+          )
+      )}
     </ol>
   )
 }
