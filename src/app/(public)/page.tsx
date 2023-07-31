3a63ea8cb1ab02b83fe0aa57a317e1e190ba1e1a
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-07-31 18:17:38 +0330

    front page slider.

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next"
 import Image from "next/image"
-import Link from "next/link"
 
 import { CheckIsMobileView } from "@core/actions/checkIsMobileView"
+import FrontPageSlider from "@/app/(public)/components/front-page-slider"
 import Search from "@/app/(public)/components/search"
 
 import logoHorizontal from "@/assets/logo-horizontal-v1-persian-light-bg.svg"
@@ -29,68 +29,7 @@ const Index = async () => {
           <Search isMobileView={isMobileView} />
         </div>
 
-        {!isMobileView && (
-          <div className="mx-auto max-w-5xl">
-            <div className="mt-24 grid grid-cols-4 gap-6">
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    ماشین آلات و تجهیزات کارگاهی
-                  </h2>
-                </Link>
-              </div>
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    مصالح ساختمانی
-                  </h2>
-                </Link>
-              </div>
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    آهن آلات، سایر فلزات و سازه
-                  </h2>
-                </Link>
-              </div>
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    تاسیسات و لوازم بهداشتی
-                  </h2>
-                </Link>
-              </div>
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    گرمایش، سرمایش و تهویه مطبوع
-                  </h2>
-                </Link>
-              </div>
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    برق، الکترونیک و دیجیتال
-                  </h2>
-                </Link>
-              </div>
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    آسانسور و پله برقی
-                  </h2>
-                </Link>
-              </div>
-              <div className="card rounded-md p-3">
-                <Link href="/search">
-                  <h2 className="text-sm font-semibold text-gray-800">
-                    درب و پنجره، پله، نرده و یراق
-                  </h2>
-                </Link>
-              </div>
-            </div>
-          </div>
-        )}
+        <FrontPageSlider />
       </div>
     </div>
   )
