8b9979166b85f097c93e436c6ad73e17095f98b5
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-08-01 14:11:06 +0330

    admin paginations.

--- src/app/admin/brands/components/Brands.tsx
+++ src/app/admin/brands/components/Brands.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import Image from "next/image"
 import { notFound, useRouter } from "next/navigation"
 import { LucideWarehouse } from "lucide-react"
@@ -11,11 +12,21 @@ import graphqlRequestClient from "@core/clients/graphqlRequestClient"
 import Loading from "@core/components/shared/Loading"
 import LoadingFailed from "@core/components/shared/LoadingFailed"
 import NoResult from "@core/components/shared/NoResult"
+import Pagination from "@/app/admin/components/Pagination"
 
 const Brands = () => {
   const { t } = useTranslation()
   const router = useRouter()
-  const { isLoading, error, data } = useGetAllBrandsQuery(graphqlRequestClient)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+
+  const { isLoading, error, data } = useGetAllBrandsQuery(
+    graphqlRequestClient,
+    {
+      indexBrandInput: {
+        page: currentPage
+      }
+    }
+  )
 
   if (isLoading) return <Loading />
   if (error) return <LoadingFailed />
@@ -23,50 +34,59 @@ const Brands = () => {
   if (!data.brands.data.length) return <NoResult entity="brand" />
 
   return (
-    <div className="card table-responsive rounded">
-      <table className="table-hover table">
-        <thead>
-          <tr>
-            <th></th>
-            <th>{t("common:brand")}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.brands.data.map(
-            (brand) =>
-              brand && (
-                <tr
-                  key={brand.id}
-                  onClick={() => router.push(`/admin/brands/${brand.id}`)}
-                >
-                  <td className="w-12">
-                    <div className="relative flex aspect-square h-12 w-12 items-center justify-center overflow-hidden rounded bg-gray-50">
-                      {brand.logoFile ? (
-                        <Image
-                          src={brand.logoFile.presignedUrl.url}
-                          alt={brand.name}
-                          fill
-                          className="object-contain"
-                        />
-                      ) : (
-                        <LucideWarehouse
-                          className="h-5 w-5 text-gray-400"
-                          strokeWidth={1.5}
-                        />
-                      )}
-                    </div>
-                  </td>
-                  <td>
-                    <span className="font-medium text-gray-800">
-                      {brand.name}
-                    </span>
-                  </td>
-                </tr>
-              )
-          )}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="card table-responsive rounded">
+        <table className="table-hover table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>{t("common:brand")}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.brands.data.map(
+              (brand) =>
+                brand && (
+                  <tr
+                    key={brand.id}
+                    onClick={() => router.push(`/admin/brands/${brand.id}`)}
+                  >
+                    <td className="w-12">
+                      <div className="relative flex aspect-square h-12 w-12 items-center justify-center overflow-hidden rounded bg-gray-50">
+                        {brand.logoFile ? (
+                          <Image
+                            src={brand.logoFile.presignedUrl.url}
+                            alt={brand.name}
+                            fill
+                            className="object-contain"
+                          />
+                        ) : (
+                          <LucideWarehouse
+                            className="h-5 w-5 text-gray-400"
+                            strokeWidth={1.5}
+                          />
+                        )}
+                      </div>
+                    </td>
+                    <td>
+                      <span className="font-medium text-gray-800">
+                        {brand.name}
+                      </span>
+                    </td>
+                  </tr>
+                )
+            )}
+          </tbody>
+        </table>
+      </div>
+      <Pagination
+        total={data.brands.lastPage}
+        page={currentPage}
+        onChange={(page) => {
+          setCurrentPage(page)
+        }}
+      />
+    </>
   )
 }
 
