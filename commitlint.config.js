60902916c3b187670b1115df461d30fa4130c6f7
mortezanjfi <mortezanajafiofficial@gmail.com>
2023-09-11 15:05:34 +0330

    conf: add huskey and update prev code formatting utils

---
+++ commitlint.config.js
@@ -0,0 +1,48 @@
+// ci: Changes to our CI configuration files and scripts (examples: CircleCi, SauceLabs)
+// feat: A new feature
+// fix: A bug fix
+// perf: A code change that improves performance
+// refactor: A code change that neither fixes a bug nor adds a feature
+// test: Adding missing tests or correcting existing tests
+// comm: Comment some code
+// merge: Fix merge conflicts
+// docs: Documentation updates
+// revert: changes that improve the security of the codebase
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "body-max-line-length": [2, "always", 200],
+    "footer-leading-blank": [1, "always"],
+    "footer-max-line-length": [2, "always", 200],
+    "header-max-length": [2, "always", 200],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"]
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "ci",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "test",
+        "conf",
+        "comm",
+        "merge",
+        "docs",
+        "revert"
+      ]
+    ]
+  }
+}
