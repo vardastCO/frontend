d394b03e3f1999281f592506a5f74762e5f291fa
mortezanjfi <mortezanajafiofficial@gmail.com>
2023-12-13 14:42:38 +0330

    fix: seller and brand profile

--- src/app/(public)/(pages)/brand/components/BrandProfile.tsx
+++ src/app/(public)/(pages)/brand/components/BrandProfile.tsx
@@ -1,19 +1,14 @@
 "use client"
 
-import { useEffect, useRef, useState } from "react"
-import Image from "next/image"
-import { notFound } from "next/navigation"
-import { digitsEnToFa } from "@persian-tools/persian-tools"
 import { useQuery } from "@tanstack/react-query"
 
 import { GetBrandQuery, IndexProductInput } from "@/generated"
 
-import Breadcrumb from "@core/components/shared/Breadcrumb"
-import { Button } from "@core/components/ui/button"
 import { getBrandQueryFn } from "@core/queryFns/brandQueryFns"
 import QUERY_FUNCTIONS_KEY from "@core/queryFns/queryFunctionsKey"
-import ProductList from "@/app/(public)/components/product-list"
-import Rating from "@/app/(public)/components/Rating"
+import BrandOrSellerProfile, {
+  BrandOrSellerEnum
+} from "@/app/(public)/components/BrandOrSellerProfile"
 
 interface BrandProfile {
   isMobileView: boolean
@@ -22,13 +17,7 @@ interface BrandProfile {
 }
 
 const BrandProfile = ({ isMobileView, args, slug }: BrandProfile) => {
-  const [imageContainerHeight, setImageContainerHeight] = useState(80)
-  const [categoriesCount, setCategoriesCount] = useState(0)
-  // const [imageBrandContainerHeight, setImageBrandContainerHeight] = useState(80)
-  const productContainerRef = useRef<HTMLDivElement>(null)
-  // const brandContainerRef = useRef<HTMLAnchorElement>(null)
-
-  const { data } = useQuery<GetBrandQuery>(
+  const query = useQuery<GetBrandQuery>(
     [QUERY_FUNCTIONS_KEY.BRAND_QUERY_KEY, { id: +slug[0] }],
     () => getBrandQueryFn(+slug[0]),
     {
@@ -36,201 +25,14 @@ const BrandProfile = ({ isMobileView, args, slug }: BrandProfile) => {
     }
   )
 
-  useEffect(() => {
-    const div = productContainerRef.current
-    if (div) {
-      setImageContainerHeight(div.clientWidth)
-    }
-    // const brandDiv = brandContainerRef.current
-    // if (brandDiv) {
-    //   setImageBrandContainerHeight(brandDiv.clientWidth)
-    // }
-  }, [])
-
-  if (!data) notFound()
-
   return (
-    <>
-      <div className="flex flex-col bg-alpha-white">
-        <Breadcrumb
-          dynamic={false}
-          items={[
-            { label: "برندها", path: "/brands", isCurrent: false },
-            {
-              label: data.brand.name,
-              path: `/brand/${data.brand.id}/${data.brand.name}`,
-              isCurrent: true
-            }
-          ]}
-        />
-        <hr className="h-px w-full bg-alpha-200" />
-      </div>
-      <div className="flex flex-col gap-y-1">
-        <div className="flex flex-col gap-y bg-alpha-white p">
-          <div className="grid grid-cols-4 items-center">
-            <div className="relative rounded-full border-2 border-alpha-400 p-0.5 shadow-lg">
-              {/* {data.brand.isBlueTik && (
-                <>
-                  <CheckBadgeIcon className="w-h-7 absolute right-1 top-0 z-20 h-7 -translate-y-1 translate-x-1 text-info" />
-                  <span className="absolute right-2 top-1 h-3 w-3 rounded-full bg-alpha-white"></span>
-                </>
-              )} */}
-              <div
-                ref={productContainerRef}
-                style={{
-                  height: imageContainerHeight
-                }}
-                className="relative z-10 h-full"
-              >
-                {data.brand?.logoFile?.presignedUrl.url ? (
-                  <Image
-                    src={data.brand?.logoFile?.presignedUrl.url as string}
-                    alt="brand"
-                    fill
-                    className="rounded-full object-contain"
-                  />
-                ) : (
-                  <Image
-                    src={"/images/brand-user.png"}
-                    alt="brand"
-                    fill
-                    className="rounded-full object-contain"
-                  />
-                )}
-              </div>
-            </div>
-            <div className="col-span-3 grid grid-cols-2">
-              <div className="flex flex-col items-center gap-y-2">
-                <h4 className="font-semibold">
-                  {digitsEnToFa(categoriesCount)}
-                </h4>
-                <p className="text-xs text-alpha-500">محصولات</p>
-              </div>
-              <div className="flex flex-col items-center gap-y-2 border-r border-alpha-200">
-                <h4 className="font-semibold">{digitsEnToFa(10)}</h4>
-                <p className="text-xs text-alpha-500">دسته‌بندی‌ها</p>
-              </div>
-            </div>
-            {/* <div className="flex flex-col items-center gap-y-2">
-            <h4 className="font-semibold">{digitsEnToFa(250)}</h4>
-            <p className="text-xs text-alpha-400">دنبال شوندگان</p>
-          </div> */}
-          </div>
-          {/* <div>
-            <p className="text-justify">{data.brand.}</p>
-          </div> */}
-        </div>
-        {/* <div className="grid grid-cols-5 items-center bg-alpha-white p">
-          <ul className="col-span-4 flex list-disc flex-col gap-y">
-            <li className="flex">
-              <span className="text-sm text-alpha-500">آدرس:</span>
-              <span className="pr text-sm">
-                {data.brand.addresses.at(0)?.address}
-              </span>
-            </li>
-            <li className="flex">
-              <span className="text-sm text-alpha-500">تلفن:</span>
-              <span className="pr text-sm">
-                {data.brand.contacts.map(({ number, code }, index) =>
-                  index === 0
-                    ? digitsEnToFa(code || "") + digitsEnToFa(number)
-                    : " - " + digitsEnToFa(code || "") + digitsEnToFa(number)
-                )}
-              </span>
-            </li>
-          </ul>
-          <Link
-            href={`https://www.google.com/maps/search/?api=1&query=${data.brand.addresses.at(
-              0
-            )?.latitude},${data.brand.addresses.at(0)?.longitude}`}
-            ref={brandContainerRef}
-            style={{
-              height: imageBrandContainerHeight
-            }}
-            target="_blank"
-            prefetch={false}
-            className="relative w-full overflow-hidden rounded-xl"
-          >
-            <Image
-              src={"/images/map.png"}
-              alt={"brand"}
-              fill
-              className="object-contain"
-            />
-          </Link>
-        </div> */}
-        <div className="grid grid-cols-3 items-center gap-y bg-alpha-white p">
-          <div className="flex flex-col items-center gap-y-2">
-            <h4 className="font-semibold">
-              <Rating rating={data.brand.rating ?? 0} size="xs" />
-            </h4>
-            <p className="text-xs text-alpha-500">{digitsEnToFa(250)} نظر</p>
-          </div>
-          <div className="flex flex-col items-center gap-y-2 border-x border-alpha-200">
-            <h4 className="text-success">{digitsEnToFa(80)}%</h4>
-            <p className="text-xs text-alpha-500">رضایت مشتری</p>
-          </div>
-          <div className="flex flex-col items-center gap-y-2">
-            <h4 className="text-success">عالی</h4>
-            <p className="text-xs text-alpha-500">عملکرد</p>
-          </div>
-        </div>
-        <div className="grid grid-cols-2 items-center gap-y bg-alpha-white">
-          <Button
-            variant="link"
-            className="!rounded-none !border-l !bg-primary-50 !py"
-          >
-            کاتالوگ
-          </Button>
-          <Button
-            variant="link"
-            className="!rounded-none !border-r !bg-secondary-50 !py"
-          >
-            لیست قیمت
-          </Button>
-        </div>
-        {/* <Tabs defaultValue="products" className="bg-alpha-white">
-          <TabsList className="w-full">
-            <TabsTrigger className="w-1/2 bg-alpha-white" value="products">
-              محصولات
-            </TabsTrigger>
-            <TabsTrigger className="w-1/2 bg-alpha-white" value="comments">
-            نظرات
-          </TabsTrigger>
-          </TabsList>
-          <TabsContent value="products">
-            <ProductList
-              setCategoriesCount={setCategoriesCount}
-              // containerType={ProductContainerType.PHOTO}
-              args={args}
-              hasFilter={false}
-              isMobileView={isMobileView}
-              selectedCategoryIds={args["categoryIds"] || undefined}
-              brandId={+slug[0]}
-            />
-          </TabsContent>
-          <TabsContent value="comments">
-          <ProductList
-            containerType={ProductContainerType.PHOTO}
-            args={args}
-            hasFilter={false}
-            isMobileView={isMobileView}
-            selectedCategoryIds={args["categoryIds"] || undefined}
-            brandId={+slug[0]}
-          />
-        </TabsContent>
-        </Tabs> */}
-        <ProductList
-          setCategoriesCount={setCategoriesCount}
-          // containerType={ProductContainerType.PHOTO}
-          args={args}
-          hasFilter={false}
-          isMobileView={isMobileView}
-          selectedCategoryIds={args["categoryIds"] || undefined}
-          brandId={+slug[0]}
-        />
-      </div>
-    </>
+    <BrandOrSellerProfile
+      type={BrandOrSellerEnum.BRAND}
+      isMobileView={isMobileView}
+      data={query.data?.brand}
+      args={args}
+      slug={slug}
+    />
   )
 }
 
--- src/app/(public)/components/BrandOrSellerProfile.tsx
+++ src/app/(public)/components/BrandOrSellerProfile.tsx
@@ -4,36 +4,48 @@ import { useEffect, useRef, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline"
 import { CheckBadgeIcon } from "@heroicons/react/24/solid"
 import { digitsEnToFa } from "@persian-tools/persian-tools"
-import { useQuery } from "@tanstack/react-query"
 
-import { GetSellerQuery, IndexProductInput } from "@/generated"
+import { GetBrandQuery, GetSellerQuery, IndexProductInput } from "@/generated"
 
 import Breadcrumb from "@core/components/shared/Breadcrumb"
-import QUERY_FUNCTIONS_KEY from "@core/queryFns/queryFunctionsKey"
-import { getSellerQueryFn } from "@core/queryFns/sellerQueryFns"
+import { Button } from "@core/components/ui/button"
 import ProductList from "@/app/(public)/components/product-list"
 import Rating from "@/app/(public)/components/Rating"
 
 export enum BrandOrSellerEnum {
-  SELLER = "SELLER",
-  BRAND = "BRAND"
+  // eslint-disable-next-line no-unused-vars
+  SELLER = "seller",
+  // eslint-disable-next-line no-unused-vars
+  BRAND = "brand"
 }
+
+export type SellerQuery = GetSellerQuery["seller"]
+export type BrandQuery = GetBrandQuery["brand"]
 
 interface BrandOrSellerProfile {
   isMobileView: boolean
   slug: Array<string | number>
   type: BrandOrSellerEnum
   args: IndexProductInput
+  data?: SellerQuery | BrandQuery
 }
+
+// function isTypeSellerQuery(data: any): data is SellerQuery {
+//   return data instanceof Object
+// }
 
 const BrandOrSellerProfile = ({
   isMobileView,
   args,
   type,
+  data,
   slug
 }: BrandOrSellerProfile) => {
+  if (!data) notFound()
+
   const [imageContainerHeight, setImageContainerHeight] = useState(80)
   const [categoriesCount, setCategoriesCount] = useState(0)
   const [imageSellerContainerHeight, setImageSellerContainerHeight] =
@@ -41,12 +53,13 @@ const BrandOrSellerProfile = ({
   const productContainerRef = useRef<HTMLDivElement>(null)
   const sellerContainerRef = useRef<HTMLAnchorElement>(null)
 
-  const { data } = useQuery<GetSellerQuery>(
-    [QUERY_FUNCTIONS_KEY.SELLER_QUERY_KEY, { id: +slug[0] }],
-    () => getSellerQueryFn(+slug[0]),
-    {
-      keepPreviousData: true
-    }
+  const phoneNumbers = data.contacts.map(
+    ({ number, code }, index) =>
+      code &&
+      number &&
+      (index === 0
+        ? digitsEnToFa(code) + digitsEnToFa(number)
+        : " - " + digitsEnToFa(code) + digitsEnToFa(number))
   )
 
   useEffect(() => {
@@ -60,7 +73,7 @@ const BrandOrSellerProfile = ({
     }
   }, [])
 
-  if (!data) notFound()
+  const isSellerQuery = () => type === BrandOrSellerEnum.SELLER
 
   return (
     <>
@@ -68,10 +81,14 @@ const BrandOrSellerProfile = ({
         <Breadcrumb
           dynamic={false}
           items={[
-            { label: "فروشندگان", path: "/sellers", isCurrent: false },
             {
-              label: data.seller.name,
-              path: `/seller/${data.seller.id}/${data.seller.name}`,
+              label: "فروشندگان",
+              path: `/${data.name}s`,
+              isCurrent: false
+            },
+            {
+              label: data.name,
+              path: `/${type}/${data.id}/${data.name}`,
               isCurrent: true
             }
           ]}
@@ -80,9 +97,9 @@ const BrandOrSellerProfile = ({
       </div>
       <div className="flex flex-col gap-y-1">
         <div className="flex flex-col gap-y bg-alpha-white p">
-          <div className="grid grid-cols-4 items-center">
-            <div className="relative rounded-full border-2 border-alpha-400 p-0.5 shadow-lg">
-              {data.seller.isBlueTik && (
+          <div className="flex items-center justify-center">
+            <div className="relative w-1/4 rounded-full border-2 border-alpha-400 p-0.5 shadow-lg">
+              {isSellerQuery() && (data as SellerQuery).isBlueTik && (
                 <>
                   <CheckBadgeIcon className="w-h-7 absolute right-1 top-0 z-20 h-7 -translate-y-1 translate-x-1 text-info" />
                   <span className="absolute right-2 top-1 h-3 w-3 rounded-full bg-alpha-white"></span>
@@ -95,9 +112,9 @@ const BrandOrSellerProfile = ({
                 }}
                 className="relative z-10 h-full"
               >
-                {data.seller?.logoFile?.presignedUrl.url ? (
+                {data?.logoFile?.presignedUrl.url ? (
                   <Image
-                    src={data.seller?.logoFile?.presignedUrl.url as string}
+                    src={data?.logoFile?.presignedUrl.url as string}
                     alt="seller"
                     fill
                     className="rounded-full object-contain"
@@ -112,7 +129,28 @@ const BrandOrSellerProfile = ({
                 )}
               </div>
             </div>
-            <div className="col-span-3 grid grid-cols-2">
+            {!isSellerQuery() && (
+              <div className="grid w-3/4 grid-rows-2 gap-x">
+                <div className=""></div>
+                <div className="flex justify-end gap-x">
+                  <Button
+                    variant="ghost"
+                    className="rounded-xl border border-primary !p-2"
+                  >
+                    کاتالوگ
+                    <ArrowDownTrayIcon className="h-5 w-5 text-primary" />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="rounded-xl border border-primary !p-2"
+                  >
+                    لیست قیمت
+                    <ArrowDownTrayIcon className="h-5 w-5 text-primary" />
+                  </Button>
+                </div>
+              </div>
+            )}
+            {/* <div className="col-span-3 grid grid-cols-2">
               <div className="flex flex-col items-center gap-y-2">
                 <h4 className="font-semibold">
                   {digitsEnToFa(categoriesCount)}
@@ -123,14 +161,29 @@ const BrandOrSellerProfile = ({
                 <h4 className="font-semibold">{digitsEnToFa(10)}</h4>
                 <p className="text-xs text-alpha-500">دسته‌بندی‌ها</p>
               </div>
-            </div>
+            </div> */}
             {/* <div className="flex flex-col items-center gap-y-2">
             <h4 className="font-semibold">{digitsEnToFa(250)}</h4>
             <p className="text-xs text-alpha-400">دنبال شوندگان</p>
           </div> */}
           </div>
-          <div>
-            <p className="text-justify">{data.seller.bio}</p>
+        </div>
+        <div className="grid auto-cols-fr grid-flow-col items-center bg-alpha-white p">
+          <div className="flex flex-col items-center gap-y-2 border-l border-alpha-200">
+            <p className="text-xs text-alpha-500">کالا</p>
+            <h4 className="">{digitsEnToFa(categoriesCount)}</h4>
+          </div>
+          <div className="flex flex-col items-center gap-y-2 border-l border-alpha-200">
+            <p className="text-xs text-alpha-500">دسته‌بندی</p>
+            <h4 className="">{digitsEnToFa(10)}</h4>
+          </div>
+          <div className="flex flex-col items-center gap-y-2 border-l border-alpha-200">
+            <p className="text-xs text-alpha-500">{digitsEnToFa(250)} نظر</p>
+            <Rating rating={data.rating ?? 0} size="xs" />
+          </div>
+          <div className="flex flex-col items-center gap-y-2">
+            <p className="text-xs text-alpha-500">عملکرد</p>
+            <h4 className="">عالی</h4>
           </div>
         </div>
         <div className="grid grid-cols-5 items-center bg-alpha-white p">
@@ -138,24 +191,20 @@ const BrandOrSellerProfile = ({
             <li className="flex">
               <span className="text-sm text-alpha-500">آدرس:</span>
               <span className="pr text-sm">
-                {data.seller.addresses.at(0)?.address}
+                {data.addresses.at(0)?.address ?? "-"}
               </span>
             </li>
             <li className="flex">
               <span className="text-sm text-alpha-500">تلفن:</span>
               <span className="pr text-sm">
-                {data.seller.contacts.map(({ number, code }, index) =>
-                  index === 0
-                    ? digitsEnToFa(code || "") + digitsEnToFa(number)
-                    : " - " + digitsEnToFa(code || "") + digitsEnToFa(number)
-                )}
+                {phoneNumbers.length ? phoneNumbers : "-"}
               </span>
             </li>
           </ul>
           <Link
-            href={`https://www.google.com/maps/search/?api=1&query=${data.seller.addresses.at(
+            href={`https://www.google.com/maps/search/?api=1&data=${data.addresses.at(
               0
-            )?.latitude},${data.seller.addresses.at(0)?.longitude}`}
+            )?.latitude},${data.addresses.at(0)?.longitude}`}
             ref={sellerContainerRef}
             style={{
               height: imageSellerContainerHeight
@@ -172,22 +221,12 @@ const BrandOrSellerProfile = ({
             />
           </Link>
         </div>
-        <div className="grid grid-cols-3 items-center gap-y bg-alpha-white p">
-          <div className="flex flex-col items-center gap-y-2">
-            <h4 className="font-semibold">
-              <Rating rating={data.seller.rating ?? 0} size="xs" />
-            </h4>
-            <p className="text-xs text-alpha-500">{digitsEnToFa(250)} نظر</p>
+        {data.bio && (
+          <div className="flex flex-col items-start bg-alpha-white p-6">
+            <h4>معرفی</h4>
+            {<p className="pt-6 text-justify">{data.bio}</p>}
           </div>
-          <div className="flex flex-col items-center gap-y-2 border-x border-alpha-200">
-            <h4 className="text-success">{digitsEnToFa(80)}%</h4>
-            <p className="text-xs text-alpha-500">رضایت مشتری</p>
-          </div>
-          <div className="flex flex-col items-center gap-y-2">
-            <h4 className="text-success">عالی</h4>
-            <p className="text-xs text-alpha-500">عملکرد</p>
-          </div>
-        </div>
+        )}
         {/* <Tabs defaultValue="products" className="bg-alpha-white">
           <TabsList className="w-full">
             <TabsTrigger className="w-1/2 bg-alpha-white" value="products">
@@ -214,7 +253,6 @@ const BrandOrSellerProfile = ({
         */}
         <ProductList
           setCategoriesCount={setCategoriesCount}
-          // containerType={ProductContainerType.PHOTO}
           args={args}
           hasFilter={false}
           isMobileView={isMobileView}
